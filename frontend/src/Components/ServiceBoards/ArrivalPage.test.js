2f757a008ab45db356fd7291d1d99c9ada15b915
jahmagnus <jahmagnus@gmail.com>
2022-06-07 18:20:07 +1000

    ...

--- frontend/src/Components/ServiceBoards/ArrivalPage.test.js
+++ frontend/src/Components/ServiceBoards/ArrivalPage.test.js
@@ -1,3 +1,6 @@
-import { screen } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
+import ArrivalPage from './Components/ServiceBoards/ArrivalPage'
 
-
+test('when button clicked, stations are cleared fro value', () => {
+    render(<ArrivalPage/>)
+})
