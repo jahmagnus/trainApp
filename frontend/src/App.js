2025e251c78189cddee5b184377742c73b9862c9
jahmagnus <jahmagnus@gmail.com>
2022-06-14 09:57:59 +1000

    tidy up app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 import Home from "./Components/Home/Home";
 import DeparturePage from "./Components/ServiceBoards/DeparturePage";
 import Login from "./Components/Login/Login";
 import SetUser from "./Components/SetUser";
-import ArrivalPage from "./Components/ServiceBoards/ArrivalPage"
-import IncidentForm from "./Components/Forms/IncidentForm/IncidentForm"
+import ArrivalPage from "./Components/ServiceBoards/ArrivalPage";
+import IncidentForm from "./Components/Forms/IncidentForm/IncidentForm";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
-
   const handleUser = (currentUser) => {
-    setUser(currentUser)
-    
-  }
+    setUser(currentUser);
+  };
 
   //check for a logged in user and set this user as the current logged in user
   useEffect(() => {
@@ -30,15 +27,15 @@ const App = () => {
   return (
     <Routes>
       {/* Public pages*/}
-      <Route path="/" element={<Login handleUser={handleUser}/>} />
+      <Route path="/" element={<Login handleUser={handleUser} />} />
 
       {/* Private pages */}
 
       <Route exact path="/departures" element={<DeparturePage user={user} />} />
       <Route exact path="/home" element={<Home user={user} />} />
       <Route exact path="/setuser" element={<SetUser user={user} />} />
-      <Route exact path="/arrivals" element={<ArrivalPage user={user}/>} />
-      <Route exact path="/incident" element={<IncidentForm user={user}/>} />
+      <Route exact path="/arrivals" element={<ArrivalPage user={user} />} />
+      <Route exact path="/incident" element={<IncidentForm user={user} />} />
 
       {/** Catch */}
       <Route path="*" element={"Page does not exist"} />
