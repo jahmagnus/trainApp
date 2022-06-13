f9a554dc7cc5159a746034970983330e0b1e376a
jahmagnus <jahmagnus@gmail.com>
2022-06-14 09:16:37 +1000

    ....

--- frontend/src/Components/Forms/IncidentForm/GeneralSection.js
+++ frontend/src/Components/Forms/IncidentForm/GeneralSection.js
@@ -10,9 +10,9 @@ const GeneralSection = ({ handleDate,handleTime, handleLocation, values }) => {
 
   return (
     <div className="container general-container">
-      <Field label="Date" type="date" name="date-time" value={values.date} onChange={handleDate}/>
-      <Field label="Time" type="time" name="time-date" value = {values.time} onChange={handleTime}/>
-      <Field label="Location" type="text" name="location" value = {values.location} onChange={handleLocation}/>
+      <Field label="Date" type="date" name="date-time" onChange={handleDate}/>
+      <Field label="Time" type="time" name="time-date" onChange={handleTime}/>
+      <Field label="Location" type="text" name="location" onChange={() => console.log('thing')}/>
 
       <div className="row button-row">
         <div className="row">
