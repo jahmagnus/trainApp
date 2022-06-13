9a43157483c981abdf665f053a091d9a12bece96
jahmagnus <jahmagnus@gmail.com>
2022-06-13 13:41:11 +1000

    textbox component added

---
+++ frontend/src/Components/Forms/TextBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextBox = () => {
+    return(
+        <div className="field">
+        <label>Text</label>
+        <textarea></textarea>
+      </div>
+    )
+}
+
+export default TextBox;
