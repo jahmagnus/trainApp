c343e8e000dd62edc790e04238e31539f78697cf
jahmagnus <jahmagnus@gmail.com>
2022-06-13 16:24:51 +1000

    ...

--- frontend/src/Components/Forms/IncidentForm/GeneralSection.js
+++ frontend/src/Components/Forms/IncidentForm/GeneralSection.js
@@ -6,13 +6,13 @@ import Prev from "../FormComponents/PrevButton";
 import Next from "../FormComponents/NextButton";
 import Submit from "../FormComponents/SubmitButton";
 
-const GeneralSection = ({ handleDate, handleTime, handleLocation }) => {
+const GeneralSection = ({ handleDate,handleTime, handleLocation, values }) => {
 
   return (
     <div className="container general-container">
-      <Field label="Date" type="date" name="date-time" onChange={handleDate}/>
-      <Field label="Time" type="time" name="time-date" />
-      <Field label="Location" type="text" name="location" />
+      <Field label="Date" type="date" name="date-time" value={values.date} onChange={handleDate}/>
+      <Field label="Time" type="time" name="time-date" value = {values.time} onChange={handleTime}/>
+      <Field label="Location" type="text" name="location" value = {values.location} onChange={handleLocation}/>
 
       <div className="row button-row">
         <div className="row">
--- frontend/src/Components/Forms/IncidentForm/IncidentForm.js
+++ frontend/src/Components/Forms/IncidentForm/IncidentForm.js
@@ -1,30 +1,27 @@
-import React, {useState} from "react";
-import {Navigate} from 'react-router-dom';
-
-
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 //css
-import './IncidentForm.css'
+import "./IncidentForm.css";
 
 //component imports
-import GeneralSection from './GeneralSection'
-import Escape from '../../Escape/EscapeHome'
-
+import GeneralSection from "./GeneralSection";
+import Escape from "../../Escape/EscapeHome";
 
 const IncidentForm = ({ user }) => {
-
-
   //state items for general section inputs
   const [pageNum, setPageNum] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [location, setLocation] = useState("");
 
+  useEffect(()=> {
+      console.log(date, time, location)
+  }, [date, time, location])
+
   //different types of state to be handled
-  
-
-
-    //check to see if user data matches the user stored in local storage
+  const values = {date, time, location};
+  //check to see if user data matches the user stored in local storage
   const storageData = localStorage.getItem("user");
   const parseUser = JSON.parse(storageData);
   if (parseUser == null) {
@@ -32,24 +29,30 @@ const IncidentForm = ({ user }) => {
     return <Navigate to="/" replace />;
   }
 
-
+ 
 
   //handle the change of inputs
   const handleDate = (e) => {
-    setDate(e.target.value);
+    setDate(e.target.value)
   }
+
+  const handleTime = (e) => {
+    setTime(e.target.value)
+  }
+
+  const handleLocation = (e) => {
+    setLocation(e.target.value)
+  }
+  
+
 
   return (
     <div className="ui centered grid">
-        <div className="twelve wide column">
-        <Escape/>
-      <form className="ui inverted form form-container">
-
-        <GeneralSection handleChange={handleDate}/>
-
-
-      </form>
-
+      <div className="twelve wide column">
+        <Escape />
+        <form className="ui inverted form form-container">
+          <GeneralSection handleTime={handleTime} handleDate ={handleDate} handleLocation={handleLocation} values={values}/>
+        </form>
       </div>
     </div>
   );
