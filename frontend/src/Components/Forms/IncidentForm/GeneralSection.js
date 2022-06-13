5bf7ba5fc482ea1458d2b756dc06fd969b8b0043
jahmagnus <jahmagnus@gmail.com>
2022-06-13 15:57:02 +1000

    ...

--- frontend/src/Components/Forms/IncidentForm/GeneralSection.js
+++ frontend/src/Components/Forms/IncidentForm/GeneralSection.js
@@ -6,25 +6,25 @@ import Prev from "../FormComponents/PrevButton";
 import Next from "../FormComponents/NextButton";
 import Submit from "../FormComponents/SubmitButton";
 
-const GeneralSection = () => {
+const GeneralSection = ({ handleDate, handleTime, handleLocation }) => {
+
   return (
-      <div className="container general-container">
-    
-      <Field label="Date" type="date" name="date-time" />
+    <div className="container general-container">
+      <Field label="Date" type="date" name="date-time" onChange={handleDate}/>
       <Field label="Time" type="time" name="time-date" />
       <Field label="Location" type="text" name="location" />
-    
-    
-        <div className="row button-row">
-        <div className="row"><Prev buttonState="disabled"/><Next buttonState=""/></div>
-        <div className="row"><Submit/></div>     
-      
+
+      <div className="row button-row">
+        <div className="row">
+          <Prev buttonState="disabled" />
+          <Next buttonState="" />
+        </div>
+        <div className="row">
+          <Submit/>
+        </div>
       </div>
     </div>
-    
   );
 };
 
 export default GeneralSection;
-
-
--- frontend/src/Components/Forms/IncidentForm/IncidentForm.js
+++ frontend/src/Components/Forms/IncidentForm/IncidentForm.js
@@ -14,6 +14,15 @@ import Escape from '../../Escape/EscapeHome'
 const IncidentForm = ({ user }) => {
 
 
+  //state items for general section inputs
+  const [pageNum, setPageNum] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [location, setLocation] = useState("");
+
+  //different types of state to be handled
+  
+
 
     //check to see if user data matches the user stored in local storage
   const storageData = localStorage.getItem("user");
@@ -22,13 +31,21 @@ const IncidentForm = ({ user }) => {
     console.log("protected page, please login", user);
     return <Navigate to="/" replace />;
   }
+
+
+
+  //handle the change of inputs
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  }
+
   return (
     <div className="ui centered grid">
         <div className="twelve wide column">
         <Escape/>
       <form className="ui inverted form form-container">
 
-        <GeneralSection/>
+        <GeneralSection handleChange={handleDate}/>
 
 
       </form>
