8970a308c55cef366ebcab02c1e298440e77f6b7
jahmagnus <jahmagnus@gmail.com>
2022-07-07 10:35:07 +1000

    ...

--- frontend/src/Components/Forms/FormComponents/NextButton.js
+++ frontend/src/Components/Forms/FormComponents/NextButton.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const Button = ({ buttonState }) => {
-
-  const buttonStyle={
-    marginLeft: "2rem"
-  }
-
+  const buttonStyle = {
+    marginLeft: "2rem",
+  };
 
   return (
-    <button style={buttonStyle} className={`ui ${buttonState} right labeled icon button`}>
+    <button
+      style={buttonStyle}
+      className={`ui ${buttonState} right labeled icon button`}
+    >
       <i className="right arrow icon"></i>
       Next
     </button>
