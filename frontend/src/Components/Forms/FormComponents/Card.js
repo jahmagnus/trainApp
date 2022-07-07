37d427b99d6ff3efe154fc805209236038575d58
jahmagnus <jahmagnus@gmail.com>
2022-07-07 10:33:34 +1000

    ...

--- frontend/src/Components/Forms/FormComponents/Card.js
+++ frontend/src/Components/Forms/FormComponents/Card.js
@@ -1,28 +1,23 @@
 import React from "react";
 
-
-import Label from '../FormComponents/Label'
+import Label from "../FormComponents/Label";
 
 function Card({ valueArray, removeValue }) {
+  const cardStyle = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "100%",
+  };
 
-    const cardStyle = {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        width: "100%"
-    }
-
-
-    
   return (
     <div className="ui cards" style={cardStyle}>
       <div className="card" style={cardStyle}>
         <div className="content">
           <div className="header"></div>
-          
+
           {valueArray.map((content) => (
-              <Label key={content} content={ content } removeValue={removeValue}/>
-            ))}
-          
+            <Label key={content} content={content} removeValue={removeValue} />
+          ))}
         </div>
       </div>
     </div>
