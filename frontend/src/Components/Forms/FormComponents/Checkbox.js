cda76a11589537ade9d14621394f2d34fb9385a0
jahmagnus <jahmagnus@gmail.com>
2022-07-07 10:33:49 +1000

    ...

--- frontend/src/Components/Forms/FormComponents/Checkbox.js
+++ frontend/src/Components/Forms/FormComponents/Checkbox.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Checkbox() {
   return (
-    
     <div className="inline field">
-    <div classname="ui checkbox">
-        
-      <input type="checkbox" tabIndex="0"/>
-      <label>Checkbox</label>
-      
+      <div classname="ui checkbox">
+        <input type="checkbox" tabIndex="0" />
+        <label>Checkbox</label>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Checkbox
+export default Checkbox;
