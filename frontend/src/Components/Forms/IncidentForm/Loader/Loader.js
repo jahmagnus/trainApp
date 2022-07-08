c45e56e8ea624df4979d0ab4fb3867da79374e24
jahmagnus <jahmagnus@gmail.com>
2022-07-08 13:30:20 +1000

    ...

--- frontend/src/Components/Forms/IncidentForm/Loader/Loader.js
+++ frontend/src/Components/Forms/IncidentForm/Loader/Loader.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 function Loader({ message }) {
+  const loaderStyle = {
+    height: "20rem",
+    textAlign: "center",
+  };
 
-    const loaderStyle = {
-        height: "20rem",
-        textAlign: "center"
-    }
-
-  return (
-    <div class="ui centered inline loader"></div>
-  );
+  return <div class="ui centered inline loader"></div>;
 }
 
 export default Loader;
