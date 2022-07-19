cf98f0b760b0d76af8f081f24e33c42f860a3c61
jahmagnus <jahmagnus@gmail.com>
2022-07-19 15:42:20 +1000

    jj

---
+++ backend/schema.js
@@ -0,0 +1,33 @@
+
+Incident Form Database schema
+
+{
+username: "testUser"
+date: "12/04/22",
+time: "17:05",
+location: "Peterborough",
+destination: "Edinburgh",
+headcode: "1S25",
+origin: "London Kings Cross",
+firstName: "John",
+surname: "Doe",
+jobTitle: "Train Guard",
+homeDepot: "KGX",
+isOffwork: "Yes",
+managerName: "James Doe",
+wasReported: "Yes",
+didAttend: "No",
+abuseTypes: ["Personal", "Abusive language"],
+factors: ["Football anti social behaviour", "Alcohol related"],
+reporterFirstName: "Joseph",
+reporterSurname: "Doe",
+reporterJob: "Train Guard",
+reporterHomeLocation: "Leeds",
+reporterManagerName: "Kenneth Doe",
+comment: "John Doe suffered verbal and physical abuse from football\n
+supporters on 1s25 - police attended train and Peterborough station"
+}
+
+
+
+
