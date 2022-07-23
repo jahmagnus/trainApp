53064155334707ad93541b445665dd41f2ba86fd
jahmagnus <jahmagnus@gmail.com>
2022-07-24 00:09:56 +1000

    retrieve forms funcitonality

--- frontend/src/Components/FormData/FormData.js
+++ frontend/src/Components/FormData/FormData.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 
-import FormTable from "./FormTable"
+import FormTable from "./FormTable";
+
+/*data to return
+date - time - location - headcode */
 
 const FormData = ({ user }) => {
   
+    const populateData = () => {
+    const currentUser = user.username;
+    
+    const data = axios({
+        method: "GET",
+        url: "http://localhost:3000/formData"
+    }).then((res) => {
+        console.log(res);
+    })
 
 
 
-    return (
-    <FormTable/>
-  )
-}
+};
 
-export default FormData
+  return <FormTable />;
+};
+
+export default FormData;
--- frontend/src/Components/FormData/FormTable.js
+++ frontend/src/Components/FormData/FormTable.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import Escape from "../Escape/EscapeHome"
+
 const FormTable = () => {
+
+    
+
   return (
-    <div>FormTable</div>
+    <div className ="ui centered grid">
+        <div className="fifteen wide column column-container">
+        <Escape/>
+    <table className="ui inverted table">
+    <tbody>
+      <tr>
+        <td>Date:</td>
+        <td>Time:</td>
+        <td>Location:</td>
+        <td>Headcode:</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
   )
 }
 
