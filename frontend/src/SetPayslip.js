6b0f6188f7b29e87e103a72f1ad2a087429ef299
jahmagnus <jahmagnus@gmail.com>
2022-08-15 14:47:07 +1000

    ...

---
+++ frontend/src/SetPayslip.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+const SetPayslip = () => {
+  
+    const [date, setDate] = useState("");
+    const [netPay, setNetPay] = useState("");
+    const [basicPay, SetBasicPay] = useState("");
+    const [totalPayments, setTotalPayments] = useState("");
+    const [incomeTax, setIncomeTax] = useState("");
+    const [date, setDate] = useState("");
+    const [date, setDate] = useState("");
+    const [date, setDate] = useState("");
+  
+    return (
+    <div>SetPayslip</div>
+  )
+}
+
+export default SetPayslip
