4a1c4a402fe3c6e761d345d83b5bed6616636196
jahmagnus <jahmagnus@gmail.com>
2022-08-15 15:10:09 +1000

    ...

--- frontend/src/SetPayslip.js
+++ frontend/src/SetPayslip.js
@@ -3,18 +3,30 @@ import axios from 'axios';
 
 const SetPayslip = () => {
   
-    const [date, setDate] = useState("");11
-    const [netPay, setNetPay] = useState("");
-    const [basicPay, SetBasicPay] = useState("");
-    const [totalPayments, setTotalPayments] = useState("");
-    const [incomeTax, setIncomeTax] = useState("");
-    const [nationalInsurance, setNationalInsurance] = useState("");
-    const [pension, setPension] = useState("");
-    const [totalDeductions, setTotalDeductions] = useState("");
+    const [registerDate, setDate] = useState("");
+    const [registerNetPay, setNetPay] = useState("");
+    const [registerBasicPay, SetBasicPay] = useState("");
+    const [registerTotalPayments, setTotalPayments] = useState("");
+    const [registerIncomeTax, setIncomeTax] = useState("");
+    const [registerNationalInsurance, setNationalInsurance] = useState("");
+    const [registerPension, setPension] = useState("");
+    const [registerTotalDeductions, setTotalDeductions] = useState("");
 
     const formStyle = {
         marginTop: "3rem",
       };
+
+    const registerPayslip = () => {
+        axios({
+            method: "POST",
+            data:{
+                date: registerDate,
+                netPay: registerNetPay,
+                basicPay: registerBasicPay,
+                
+            }
+        })
+    }
   
     return (
     <div>SetPayslip</div>
