9dfcdd040b881c12ace7182b5d0e362c8f603cb6
jahmagnus <jahmagnus@gmail.com>
2022-08-19 11:02:54 +1000

    ...

--- frontend/src/Components/Payslip/Dropdown.js
+++ frontend/src/Components/Payslip/Dropdown.js
@@ -1,27 +1,25 @@
 import React from "react";
 
-const Dropdown = () => {
-
-    //years for the dropdown menu to be populated with. 
-    const years = [
-        {label: "2022", value: "2022"},
-        {label: "2021", value: "2021"},
-        {label: "2020", value: "2020"},
-        {label: "2019", value: "2019"},
-        {label: "2018", value: "2018"}, 
-    ]
-
-
-    
+const Dropdown = ({ getPayslips, user }) => {
+  //years for the dropdown menu to be populated with.
+  const years = [
+    { label: "2022", value: "2022" },
+    { label: "2021", value: "2021" },
+    { label: "2020", value: "2020" },
+    { label: "2019", value: "2019" },
+    { label: "2018", value: "2018" },
+  ];
 
   return (
-    <select>
-      <option value="">Select year</option>
-      {/* map the values from the 'years' array of objects to the dropdown */}
-      {years.map((year) => 
-        <option value={year.value}> {year.label}</option>
-      )}
-    </select>
+    <div>
+      <select>
+        <option value="">Select year</option>
+        {/* map the values from the 'years' array of objects to the dropdown */}
+        {years.map((year) => (
+          <option value={year.value}> {year.label}</option>
+        ))}
+      </select>
+    </div>
   );
 };
 
--- frontend/src/Components/Payslip/Payslip.js
+++ frontend/src/Components/Payslip/Payslip.js
@@ -3,10 +3,9 @@ import axios from "axios";
 import { Navigate } from "react-router-dom";
 
 import Dropdown from "./Dropdown";
-import Home from "../Escape/EscapeHome"
+import Home from "../Escape/EscapeHome";
 
-const Payslip = ( {user} ) => {
-
+const Payslip = ({ user }) => {
   //check to see if there is a user in local storage
   const storageData = localStorage.getItem("user");
   const parseUser = JSON.parse(storageData);
@@ -15,12 +14,26 @@ const Payslip = ( {user} ) => {
     return <Navigate to="/" replace />;
   }
 
+  //function will return payslips for a given year
+  //defined in this class but actually called in the dropdown component onChange
+  const getPayslips = (username, year) => {
+    
+    axios({
+        method: "GET", 
+        url: "http://localhost:3000/payslipData"
+    }).then((res) => {
+
+        //for the moment, console log the results of the request
+        console.log(res.data);
+    })
+
+  }
 
   return (
     <div className="ui centered grid">
-        <div className="container">
-            <Home />
-      <Dropdown />
+      <div className="container">
+        <Home />
+        <Dropdown onChange={getPayslips} user={user}/>
       </div>
     </div>
   );
